b0f68c986b03dce8d1bdb01f16708c94ac2cf741
Valentin Da silva <v.dasilva11d@gmail.com>
2023-04-19 15:37:21 +0200

    added tests for chatbot but i'm tokenless

---
+++ spec/chatest_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../lib/chatest'
+
+
+describe "the login_openai method" do
+  it "should return response, and response is not nil" do
+    expect(login_openai).not_to be_nil
+  end
+end
